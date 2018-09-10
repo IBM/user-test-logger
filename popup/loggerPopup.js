8d115bbdac0d1fe9c052efd3e30e43ac3d4a51cf
Felipe-Ferreira1 <felipe.ferreira1@ibm.com>
2018-09-10 10:37:10 -0300

    month fixed, download all removed

--- popup/loggerPopup.js
+++ popup/loggerPopup.js
@@ -5,7 +5,7 @@ function clicksHandler(event){
 		var blob = new Blob([JSON.stringify(loggerPack, null, '\t')], {type: "text/json;charset=utf-8"});
 		
 		var date = new Date(); 
-        var fileName = "" + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "-log.json";
+        var fileName = "" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "-log.json";
 			
 		saveAs(blob, fileName);
 	}
