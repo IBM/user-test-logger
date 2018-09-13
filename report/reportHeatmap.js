fddc071980d587b6aaecc664b860f9c66b033b68
Felipe-Ferreira1 <felipe@DESKTOP-774POF2.localdomain>
2018-09-13 18:37:26 +0000

    Gaussian function applied to heatmap

--- report/reportHeatmap.js
+++ report/reportHeatmap.js
@@ -207,24 +207,19 @@ function heatMap(loggerPack){
 			    .append('svg')
 				.attr('width', wLog)
 				.attr('height', hLog);
-				
-	var blur = svg.append("defs")
-					    .append("filter")
-					    .attr("id", "blur")
-					    .append("feGaussianBlur")
-					    .attr("stdDeviation", 5);
 						
 	var g = svg.append('g');
 	
 	var green = 255;
 	var red = 0;
 	
-	var square = 20;
+	var square = 10;
 	var mapWidth = Math.floor(wLog/square);
 	var mapHeight = Math.floor(hLog/square);
 					  
 	var points = [];
 	var map = [];
+	
 	for(let i = 0; i < mapWidth; i++){
 		map.push([]);
 		for(let j = 0; j < mapHeight; j++){
@@ -232,7 +227,7 @@ function heatMap(loggerPack){
 				'color': {'red': 0, 'green': 255},
 				'x': i*square,
 				'y': j*square,
-				'fixation': 0
+				'fixation': 1 
 			});
 		}
 	}
@@ -252,50 +247,38 @@ function heatMap(loggerPack){
 				 'green': green 
 		};
 	}
-		
+	
+	function createMask( r, sigma ){
+		mask = [] ;
+		s = r * sigma * sigma ;
+		for( var i = 0 ; i <= 2 * r; i++ ){
+			mask[i] = [] ;
+			for( var j = 0 ; j <= 2 * r; j++ ){
+				mask[i][j] = 0 ;
+				x = i - r ;
+				y = j - r ;
+				if( x * x + y * y <= r * r ){
+					mask[i][j] = Math.pow( Math.E, ( ( x * x + y * y ) * -1 ) / s ) / ( Math.PI * s ) ;
+					mask[i][j] = mask[i][j].toFixed( 3 ) ;
+				}
+			}
+		}
+		return mask ;
+	}
+
+	radius = 10 ;
+	mask = createMask( radius, 2 ) ;
+
 	for( var k = 0 ; k < idtFixations.length ; k++ ){
 			
 		var iIndex = Math.min( Math.max( Math.round( idtFixations[k].x/square ), 0 ), map.length - 1 ) ;
 		var jIndex = Math.min( Math.max( Math.round( idtFixations[k].y/square ), 0 ), map[iIndex].length - 1 ) ;
 		
-		var v = 1 ;	
-			
-		map[iIndex][jIndex].fixation+= v ;	
-	
-		try{ map[iIndex-1][jIndex].fixation+= v ; }catch(e){}
-		try{ map[iIndex][jIndex-1].fixation+= v ; }catch(e){}
-		try{ map[iIndex+1][jIndex].fixation+= v ; }catch(e){}
-		try{ map[iIndex][jIndex+1].fixation+= v ; }catch(e){}
-			
-		try{ map[iIndex-1][jIndex-1].fixation+= v * 0.64 ; }catch(e){}
-		try{ map[iIndex+1][jIndex+1].fixation+= v * 0.64 ; }catch(e){}
-		try{ map[iIndex-1][jIndex+1].fixation+= v * 0.64 ; }catch(e){}
-		try{ map[iIndex+1][jIndex-1].fixation+= v * 0.64 ; }catch(e){}
-
-		try{ map[iIndex-2][jIndex].fixation+= v * 0.32 ; }catch(e){}
-		try{ map[iIndex][jIndex-2].fixation+= v * 0.32 ; }catch(e){}
-		try{ map[iIndex+2][jIndex].fixation+= v * 0.32 ; }catch(e){}
-		try{ map[iIndex][jIndex+2].fixation+= v * 0.32 ;}catch(e){}
-
-		try{ map[iIndex-1][jIndex-2].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex-2][jIndex-1].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex-1][jIndex+2].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex-2][jIndex+1].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex+1][jIndex-2].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex+2][jIndex-1].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex+1][jIndex+2].fixation+= v * 0.18 ;}catch(e){}
-		try{ map[iIndex+2][jIndex+1].fixation+= v * 0.18 ;}catch(e){}
-
-		try{ map[iIndex-3][jIndex].fixation+= v * 0.05 ; }catch(e){}
-		try{ map[iIndex][jIndex-3].fixation+= v * 0.05 ; }catch(e){}
-		try{ map[iIndex+3][jIndex].fixation+= v * 0.05 ; }catch(e){}
-		try{ map[iIndex][jIndex+3].fixation+= v * 0.05 ;}catch(e){}
-
-		try{ map[iIndex-2][jIndex-2].fixation+= v * 0.05 ; }catch(e){}
-		try{ map[iIndex+2][jIndex+2].fixation+= v * 0.05 ; }catch(e){}
-		try{ map[iIndex-2][jIndex+2].fixation+= v * 0.05; }catch(e){}
-		try{ map[iIndex+2][jIndex-2].fixation+= v * 0.05 ;}catch(e){}
-			
+		for( i = -1 * radius; i <= radius; i++ ){
+			for( j = -1 * radius; j <= radius; j++ ){
+				try{ map[iIndex+i][jIndex+j].fixation += 1 * mask[i+radius][j+radius] / mask[Math.round(radius/2)][Math.round(radius/2)]; }catch(e){}	
+			}
+		}
 	}
 				  
 	for(let i = 0; i < mapWidth; i++){
@@ -307,54 +290,43 @@ function heatMap(loggerPack){
 			});
 		}
 	}
-				  
-	points.sort(function(a, b){return a.fixation-b.fixation});
-	var min = points[0].fixation;
-	var max = points[points.length - 1].fixation;
 	
-	var scale = d3.scaleLinear().domain([0, max*0.5, max]).range(["green", "yellow", "red"]);			  
-					   
-	var circles = g.selectAll('circle2')
+	var min = points[0].fixation;
+	var max = points[0].fixation;
+	
+	for(let obj of points){
+		
+		if(max < obj.fixation){
+			max = obj.fixation;
+		}
+		if(min > obj.fixation){
+			min = obj.fixation;
+		}
+		
+	}
+				  
+	var scale = d3.scaleLinear().domain([max*0.01, max*0.5, max]).range(["green", "yellow", "red"]);			  
+	
+	var rects = g.selectAll('rect')
 				   .data(points)
 				   .enter()
-				   .append('circle')
-				   .attr('cx', function(d){
+				   .append('rect')
+				   .attr('x', function(d){
 						return d.x;
 			       })
-				   .attr('cy', function(d){
+				   .attr('y', function(d){
 						return d.y;
 				   })
-				   .attr('r', square/2 + 10)
+				   .attr('width', square )
+				   .attr('height', square )
 				   .attr('fill', function(d){
 						return scale(d.fixation);
 				   })
 				   .style('opacity', function(d){
-						if( d.fixation > 0.05 ){ return 0.25 ; }
+						if( d.fixation > 0.05 ){ return 1.0 ; }
 						return 0 ;
-				   })
-				   .attr('filter', 'url(#blur)');
+				   });
 									
-}
-
-function heatmapT(backPage){
-	var w = document.body.clientWidth;
-	var h = document.body.clientHeight;
-	
-	var svg = d3.select('body')
-				.append('svg')
-				.attr('width', w)
-				.attr('height', h);
-	
-	var blurCircle = svg.append("defs")
-					    .append("filter")
-					    .attr("id", "blur")
-					    .append("feGaussianBlur")
-					    .attr("stdDeviation", 2);
-						
-	var circles = svg.selectAll('circle')
-					 .data(avgData)
-					 .enter()
-					 .append('circle');
 }
 
 function init(backPage){
