4af28e51fc9f6748cc3fb0c91d42fdc28dec18bf
Felipe-Ferreira1 <felipe@DESKTOP-774POF2.localdomain>
2019-01-03 18:26:38 +0000

    patterns updated - incident nodes table

--- report/patterns.js
+++ report/patterns.js
@@ -94,17 +94,201 @@ function showHide(e){
 	function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-async function demo() {
-  console.log('Taking a break...');
-  await sleep(2000);
-  console.log('Two seconds later');
-  tooltip.style.visibility = "visible";
+	
 }
 
-demo();
+function sheetsIncident(nodes, cardinality){
+	var table = document.getElementById("jobsTableIncident");
+	var content = document.getElementById("content");
 	
-	
+	for(let node of nodes){
+		
+		if(node.sam == "incident"){
+		var links = node.links;
+		
+		var sources = [], targets = [];
+		
+		for(let x of links){
+			//console.log(x);
+			if(x.source == node.id && x.target != node.id){
+				targets.push(x.targetReference);
+			}
+			else if(x.target == node.id && x.source != node.id){
+				sources.push(x.sourceReference);
+			}
+			else{
+				/////////THINK
+				sources.push(x.sourceReference);
+			}
+			
+		}
+		
+		var min, max;
+		if(sources.length < targets.length){
+			min = [sources.length, "sources"];
+			max = [targets.length, "targets"];
+		}
+		else{
+			max = [sources.length, "sources"];
+			min = [targets.length, "targets"];
+		}
+		
+		var diff = max[0] - min[0];
+		
+		var tooltip, tooltiptxt;
+		var description, linebreak;
+		var boxSource, box, boxTarget;
+		var ts, tb, tt;
+		var sheetEoi, sheetSource, sheetTarget;
+		var row, rowSource, rowTarget;
+		var size = 50*max[0];
+		
+		
+		row = document.createElement("tr");
+		table.appendChild(row);
+		
+		//boxSource
+		boxSource = document.createElement("td");
+		boxSource.className = "boxSource";
+		boxSource.height = size;
+		row.appendChild(boxSource);
+		
+		ts = document.createElement("table");
+		ts.className = "innerTable";
+		ts.style.height = size + 'px';
+		boxSource.appendChild(ts);
+		
+		//box
+		box = document.createElement("td");
+		box.className = "box";
+		box.height = size;
+		row.appendChild(box);
+		
+		
+		tb = document.createElement("table");
+		tb.className = "innerTable";
+		tb.style.height = size + 'px';
+		box.appendChild(tb);
+		
+		//boxTarget
+		boxTarget = document.createElement("td");
+		boxTarget.className = "boxTarget";
+		boxTarget.height = size;
+		row.appendChild(boxTarget);
+		
+		tt = document.createElement("table");
+		tt.className = "innerTable";
+		tt.style.height = size + 'px';
+		boxTarget.appendChild(tt);
+		
+
+		//Node in box
+		row = document.createElement("tr");
+		row.className = "innerRow";
+		tb.appendChild(row);
+		
+		sheetEoi = document.createElement("td");
+		sheetEoi.className = "eoi";
+		sheetEoi.title = "Event: " + node.eventName + 
+							"\nOccurrences: " + node.occurrences +
+							"\nTab: " + node.tabId + 
+							"\nPath: " + node.path + 
+							"\nElement Id: " + node.elementId + 
+							"\nMean Distance: " + node.meanDistance.toFixed(2) + 
+							"\nMean Time: " + getTimestamp(node.meanTimestamp) +
+							"\nSam: " + node.sam;
+		row.appendChild(sheetEoi);
+		description = document.createTextNode(node.eventName + '@' + node.elementId);
+		sheetEoi.appendChild(description);
+		
+		var j = 0, nodeSource, nodeTarget;
+		while(j < min[0]){
+			
+			nodeSource = sources[j];
+			nodeTarget = targets[j];
+			
+			rowSource = document.createElement("tr");
+			rowSource.className = "innerRow";
+			ts.appendChild(rowSource);
+
+			sheetSource = document.createElement("td");
+			sheetSource.className = "source";
+			sheetSource.title = "Event: " + nodeSource.eventName + 
+								"\nOccurrences: " + nodeSource.occurrences +
+								"\nTab: " + nodeSource.tabId + 
+								"\nPath: " + nodeSource.path + 
+								"\nElement Id: " +  nodeSource.elementId +
+								"\nMean Distance: " + nodeSource.meanDistance.toFixed(2) + 
+								"\nMean Time: " + getTimestamp(nodeSource.meanTimestamp) +
+								"\nSam: " + nodeSource.sam;
+			rowSource.appendChild(sheetSource);
+			description = document.createTextNode(nodeSource.eventName + '@' + nodeSource.elementId);
+			sheetSource.appendChild(description);
+			
+			rowTarget = document.createElement("tr");
+			rowTarget.className = "innerRow";
+			tt.appendChild(rowTarget);
+			
+			sheetTarget = document.createElement("td");
+			sheetTarget.className = "target";
+			sheetTarget.title = "Event: " + nodeTarget.eventName + 
+								"\nOccurrences: " + nodeTarget.occurrences +
+								"\nTab: " + nodeTarget.tabId + 
+								"\nPath: " + nodeTarget.path + 
+								"\nElement Id: " + nodeTarget.elementId +
+								"\nMean Distance: " + nodeTarget.meanDistance.toFixed(2) + 
+								"\nMean Time: " + getTimestamp(nodeTarget.meanTimestamp) +
+								"\nSam: " + nodeTarget.sam;
+			rowTarget.appendChild(sheetTarget);
+			description = document.createTextNode(nodeTarget.eventName + '@' + nodeTarget.elementId);
+			sheetTarget.appendChild(description);
+			
+		
+			j++;
+		}
+			
+		var array, className, t, sheet;
+		if(max[1] == "sources"){
+			array = sources;
+			className = "source";
+			t = ts;
+		}
+		else{
+			array = targets;
+			className = "target";
+			t = tt;
+		}
+		
+		var nodex;
+		while(j < max[0]){
+			
+			nodex = array[j];
+			
+			row = document.createElement("tr");
+			row.className = "innerRow";
+			t.appendChild(row);
+
+			sheet = document.createElement("td");
+			sheet.className = className;
+			sheet.title = "Event: " + nodex.eventName + 
+							"\nOccurrences: " + nodex.occurrences +
+							"\nTab: " + nodex.tabId + 
+							"\nPath: " + nodex.path + 
+							"\nElement Id: " + nodex.elementId +
+							"\nMean Distance: " + nodex.meanDistance.toFixed(2) + 
+							"\nMean Time: " + getTimestamp(nodex.meanTimestamp) +
+							"\nSam: " + nodex.sam;
+			sheet.width = "100%";
+			row.appendChild(sheet);
+			description = document.createTextNode(nodex.eventName + '@' + nodex.elementId);
+			sheet.appendChild(description);
+			
+			
+		
+			j++;
+		}
+		}
+	}
 }
 
 function sheets(nodes, i, cardinality){
@@ -114,8 +298,8 @@ function sheets(nodes, i, cardinality){
 	var k = cardinality;	
 	while(k > i){
 		
-		node = nodes[k - 1];
-		links = node.links;
+		var node = nodes[k - 1];
+		var links = node.links;
 		
 		console.log(node);
 		
@@ -214,44 +398,6 @@ function sheets(nodes, i, cardinality){
 		description = document.createTextNode(node.eventName + '@' + node.elementId);
 		sheetEoi.appendChild(description);
 		
-		//tooltip in box	
-		/*		
-		tooltiptxt = document.createElement("span");
-		tooltiptxt.className = "tooltiptxt";
-		
-		description = document.createTextNode("Event: " + node.eventName);
-		tooltiptxt.appendChild(description);
-		linebreak = document.createElement('br');
-		tooltiptxt.appendChild(linebreak);
-		
-		description = document.createTextNode("Tab: " + node.tabId);
-		tooltiptxt.appendChild(description);
-		linebreak = document.createElement('br');
-		tooltiptxt.appendChild(linebreak);
-		
-		description = document.createTextNode("Path: " + node.path);
-		tooltiptxt.appendChild(description);
-		linebreak = document.createElement('br');
-		tooltiptxt.appendChild(linebreak);
-		
-		description = document.createTextNode("Mean Distance: " + node.meanDistance);
-		tooltiptxt.appendChild(description);
-		linebreak = document.createElement('br');
-		tooltiptxt.appendChild(linebreak);
-		
-		description = document.createTextNode("Mean Time: " + node.meanTimestamp);
-		tooltiptxt.appendChild(description);
-		linebreak = document.createElement('br');
-		tooltiptxt.appendChild(linebreak);
-		
-		sheetEoi.appendChild(tooltiptxt);
-		*/
-		//Event Listener - tooltip
-	    //sheetEoi.addEventListener("mouseover", show);
-		//sheetEoi.addEventListener("mousemove", showHide);
-		//sheetEoi.addEventListener("mouseout", hide);
-		
-		
 		var j = 0, nodeSource, nodeTarget;
 		while(j < min[0]){
 			
@@ -345,9 +491,9 @@ function sheets(nodes, i, cardinality){
 }
 
 function patterns(backPage){
-	graph = createGraph(backPage.loggerPack);
-	nodes = graph.nodes;
-	links = graph.links;
+	var graph = createGraph(backPage.loggerPack);
+	var nodes = graph.nodes;
+	var links = graph.links;
 	
 	getReference(nodes, links);
 		
@@ -376,6 +522,7 @@ function patterns(backPage){
 	}
 	
 	sheets(nodes, i, cardinality);
+	sheetsIncident(nodes, cardinality);
 	console.log("patterns");
 }
  
