7b52fc84aecdd8268e91b930890e84ae8f74e061
Felipe-Ferreira1 <felipe@DESKTOP-774POF2.localdomain>
2019-01-21 16:48:26 +0000

    heatmap for each pageview - download is not configured - first try to get a screenshot from the page

--- report/heatmap.js
+++ report/heatmap.js
@@ -151,11 +151,102 @@ function heatMap(loggerPack){
 		}
 		return fixationArray;
 	}
+		
+	var string = '';
 	
 	var lastPageView = {};
 	var wLastPageView = 0;
 	var hLastPageView = 0;
 	
+	var maps = {};
+	var wdimensions = {};
+	var hdimensions = {};
+	
+	for(let line of log){		
+		currentTime = line[2];
+		name = line[3];
+		popup = line[7].indexOf('loggerPopup.html');
+		
+		if(name == 'pageview' && popup != -1){
+			pluginTab = line[0];
+		}
+		else if(name == 'pageview'){
+			index = line[7].indexOf('|');
+			string = line[7].slice(0, index);
+			lastPageView[line[0]] = string;
+			
+			if(maps[line[0]] == undefined){
+				maps[line[0]] = {};
+				maps[line[0]][string] = true;
+			}
+			else{
+				maps[line[0]][string] = true;
+			}
+			
+			index = line[7].indexOf('|');
+			coord = line[7].slice(index + 1, line[7].length);
+			
+			index = coord.indexOf('x');
+			wTemp = Number( coord.slice(0, index) );
+			hTemp = Number( coord.slice(index + 1, line[7].length) );
+			
+			
+			if(wdimensions[line[0]] == undefined && hdimensions[line[0]] == undefined){
+				wdimensions[line[0]] = {};
+				wdimensions[line[0]][string] = wTemp;
+			
+				hdimensions[line[0]] = {};
+				hdimensions[line[0]][string] = hTemp;
+			}
+			else{
+				wdimensions[line[0]][string] = wTemp;
+				hdimensions[line[0]][string] = hTemp;
+			}
+			
+		}
+		
+		if(line[3] == 'resize'){
+			
+			coord = line[7];
+			index = coord.indexOf('x');
+			wTemp = Number( coord.slice(0, index) );
+			hTemp = Number( coord.slice(index + 1, coord.length) );
+			
+			if(wTemp > wdimensions[line[0]][lastPageView[line[0]]]){
+				wdimensions[line[0]][lastPageView[line[0]]] = wTemp;
+			}
+			
+			if(hTemp > hdimensions[line[0]][lastPageView[line[0]]]){
+				hdimensions[line[0]][lastPageView[line[0]]] = hTemp;
+			}
+		}
+		
+		if(name == 'mousemove' && pluginTab != line[0] && line[0] != 0){ //trying the line 0
+			
+			index = line[7].indexOf('|');
+			//window coordinates
+			//coord = line[7].slice(index + 1, line[7].length);
+			
+			//page coordinates
+			coord = line[7].slice(0, index);
+			
+			index = coord.indexOf('x');
+			object = {
+				'x': coord.slice(0,index),
+				'y': coord.slice(index + 1, coord.length), 
+				'fixation': currentTime - previousTime,
+				'pageview': lastPageView[line[0]],
+				'tab': line[0]
+			};
+			mouseTrack.push(object);
+		}
+		previousTime = line[2];
+		
+	}	
+	
+	
+	
+	/*
 	for(let line of log){		
 		if(line[3] == 'pageview' && line[7].indexOf('loggerPopup.html') == -1){
 			
@@ -213,81 +304,50 @@ function heatMap(loggerPack){
 			}
 		}*/
 		
-	}
+	//}
 	
-	for(let line of log){		
-		currentTime = line[2];
-		name = line[3];
-		popup = line[7].indexOf('loggerPopup.html');
-		
-		if(name == 'pageview' && popup != -1){
-			pluginTab = line[0];
-		}
-		else if(name == 'pageview'){
-			index = line[7].indexOf('|');
-			lastPageView[line[0]] = line[7].slice(0, index);
-		}
-		
-		if(name == 'mousemove' && pluginTab != line[0]){
-			
-			index = line[7].indexOf('|');
-			//window coordinates
-			//coord = line[7].slice(index + 1, line[7].length);
-			
-			//page coordinates
-			coord = line[7].slice(0, index);
-			
-			index = coord.indexOf('x');
-			object = {
-				'x': coord.slice(0,index),
-				'y': coord.slice(index + 1, coord.length), 
-				'fixation': currentTime - previousTime,
-				'pageview': lastPageView[line[0]],
-				'tab': line[0]
-			};
-			mouseTrack.push(object);
-		}
-		previousTime = line[2];
-		
-	}
+	console.log(maps);
+	
 	var off = 0;
 	var idtFixations = idt(mouseTrack, 10, 50);
 	for(let a of idtFixations){
 		a.y -= off;
 	}
-		
-	var mousePath = d3.line()
-				 .x(function(d){ return d.x; })
-				 .y(function(d){ return d.y; });
-				 
-	var svg = d3.select('body')
-			    .append('svg')
-				.attr('width', wLog)
-				.attr('height', hLog);
-						
-	var g = svg.append('g');
 	
 	var green = 255;
 	var red = 0;
 	
 	var square = 10;
-	var mapWidth = Math.floor(wLog/square);
-	var mapHeight = Math.floor(hLog/square);
-					  
-	var points = [];
-	var map = [];
+	//var mapWidth = Math.floor(wLog/square);
+	//var mapHeight = Math.floor(hLog/square);
+
 	
-	for(let i = 0; i < mapWidth; i++){
-		map.push([]);
-		for(let j = 0; j < mapHeight; j++){
-			map[i].push({
-				'color': {'red': 0, 'green': 255},
-				'x': i*square,
-				'y': j*square,
-				'fixation': 1 
-			});
-		}
+	//separating the maps by tab and pageview
+	for(let tab in maps){
+		for(let pageview in maps[tab]){
+	
+			maps[tab][pageview] = [];
+			
+			mapWidth = Math.floor(wdimensions[tab][pageview]/square);
+			mapHeight = Math.floor(hdimensions[tab][pageview]/square);
+	
+			for(let i = 0; i < mapWidth; i++){
+				maps[tab][pageview].push([]);
+				for(let j = 0; j < mapHeight; j++){
+					maps[tab][pageview][i].push({
+						'color': {'red': 0, 'green': 255},
+						'x': i*square,
+						'y': j*square,
+						'fixation': 1 
+					});
+				}
+			}
+		
+		}	
 	}
+	
+	console.log(wdimensions);
+	console.log(hdimensions);
 	
 	function calcColor(fixation){
 		
@@ -325,71 +385,188 @@ function heatMap(loggerPack){
 
 	radius = 10 ;
 	mask = createMask( radius, 2 ) ;
-
+	
 	for( var k = 0 ; k < idtFixations.length ; k++ ){
+		
+		var idtF = idtFixations[k];console.log('aaa');
+		
+		if(idtF.pageview != ''){
 			
-		var iIndex = Math.min( Math.max( Math.round( idtFixations[k].x/square ), 0 ), map.length - 1 ) ;
-		var jIndex = Math.min( Math.max( Math.round( idtFixations[k].y/square ), 0 ), map[iIndex].length - 1 ) ;
+			var iIndex = Math.min( Math.max( Math.round( idtF.x/square ), 0 ), maps[idtF.tab][idtF.pageview].length - 1 ) ;
+			var jIndex = Math.min( Math.max( Math.round( idtF.y/square ), 0 ), maps[idtF.tab][idtF.pageview][iIndex].length - 1 ) ;
 		
-		for( i = -1 * radius; i <= radius; i++ ){
-			for( j = -1 * radius; j <= radius; j++ ){
-				try{
-					map[iIndex+i][jIndex+j].fixation += 1 * mask[i+radius][j+radius] / mask[Math.round(radius/2)][Math.round(radius/2)]; 
+			for( i = -1 * radius; i <= radius; i++ ){
+				for( j = -1 * radius; j <= radius; j++ ){
+					try{
+						maps[idtF.tab][idtF.pageview][iIndex+i][jIndex+j].fixation += 1 * mask[i+radius][j+radius] / mask[Math.round(radius/2)][Math.round(radius/2)]; 
 					
-				}catch(e){}	
+					}catch(e){}	
+				}
 			}
 		}
 	}
-				  
-	for(let i = 0; i < mapWidth; i++){
-		for(let j = 0; j < mapHeight; j++){
-			if(map[i][j].fixation > 1){
-				points.push({
-					'x': map[i][j].x,
-					'y': map[i][j].y,
-					'fixation': map[i][j].fixation
-				});
+		
+	console.log(maps);
+	
+	var points = {};
+	
+	for(let tab in maps){
+		
+		for(let pageview in maps[tab]){
+			
+			if(points[tab] == undefined){
+				points[tab] = {};
+				points[tab][pageview] = [];
 			}
-		}
-	}
-	
-	var min = points[0].fixation;
-	var max = points[0].fixation;
-	
-	for(let obj of points){
-		
-		if(max < obj.fixation){
-			max = obj.fixation;
-		}
-		if(min > obj.fixation){
-			min = obj.fixation;
-		}
-		
-	}
+			else{
+				points[tab][pageview] = [];
+			}
+			
+			mapWidth = Math.floor(wdimensions[tab][pageview]/square);
+			mapHeight = Math.floor(hdimensions[tab][pageview]/square);
 				  
-	//var scale = d3.scaleLinear().domain([max*0.1, max*0.5, max]).range(["white", "blue", "purple"]);			  
-	var scale = d3.scaleLinear().domain([max*0.05, max*0.1, max*0.2, max*0.55, max]).range(["white", "lightgreen", "green", "yellow", "red"]);
+			for(let i = 0; i < mapWidth; i++){
+				for(let j = 0; j < mapHeight; j++){
+					if(maps[tab][pageview][i][j].fixation > 1){
+						points[tab][pageview].push({
+							'x': maps[tab][pageview][i][j].x,
+							'y': maps[tab][pageview][i][j].y,
+							'fixation': maps[tab][pageview][i][j].fixation
+						});
+					}
+				}
+			}
 	
-	var rects = g.selectAll('rect')
-				   .data(points)
-				   .enter()
-				   .append('rect')
-				   .attr('x', function(d){
-						return d.x;
-			       })
-				   .attr('y', function(d){
-						return d.y;
-				   })
-				   .attr('width', square )
-				   .attr('height', square )
-				   .attr('fill', function(d){
-						return scale(d.fixation);
-				   })
-				   .style('opacity', function(d){
-						if( d.fixation > 0.05 ){ return 1.0 ; }
-						return 0 ;
-				   });
+		}
+	}
+		
+	
+	var body = document.getElementsByTagName("BODY")[0]; 
+	
+	for(let tab in points){
+		
+		
+		var header1 = document.createElement("div");
+		header1.style.borderTop = "2px solid #c1c1c1";
+		header1.style.borderBottom = "2px solid #c1c1c1";
+		header1.style.fontSize = "1.1em";
+		header1.style.textAlign = "center";
+		header1.style.fontWeight = "bold";
+		header1.style.padding = "10px 5px 10px 5px";
+		
+		var textNode = document.createTextNode('Tab ' + tab);
+		header1.appendChild(textNode);
+		body.appendChild(header1);
+		
+		for(let pageview in points[tab]){	
+		
+			console.log(pageview);
+	
+			var min = points[tab][pageview][0].fixation;
+			var max = points[tab][pageview][0].fixation;
+				
+			for(let obj of points[tab][pageview]){
+			
+				if(max < obj.fixation){
+					max = obj.fixation;
+				}
+				if(min > obj.fixation){
+					min = obj.fixation;
+				}
+			
+			}
+								  
+			var mousePath = d3.line()
+					 .x(function(d){ return d.x; })
+					 .y(function(d){ return d.y; });
+			
+			
+			var container = document.createElement("div");
+			container.className = "container-pageview";
+			container.id = tab + pageview;
+			
+			header1 = document.createElement("div");
+			header1.className = "pageview-heading";
+			
+			//header1.style.borderTop = "1px solid #efefef";
+			header1.style.borderBottom = "1px solid #c1c1c1";
+			header1.style.fontSize = "1em";
+			header1.style.textAlign = "center";
+			//header1.style.fontWeight = "bold";
+			header1.style.padding = "10px 5px 10px 5px";
+			
+			var arrow = document.createElement("span");
+			arrow.className = "arrow-up";
+			
+			arrow.innerHTML = "&#9650;";
+			arrow.style.fontSize = "0.8em";
+			arrow.style.marginRight = "2px";
+			arrow.style.cursor = "pointer";
+			arrow.style.display = "none";
+			header1.appendChild(arrow);
+			
+			arrow = document.createElement("span");
+			arrow.className = "arrow-down";
+			
+			arrow.innerHTML = "&#9660;";
+			arrow.style.fontSize = "0.8em";
+			arrow.style.marginRight = "2px";
+			arrow.style.cursor = "pointer";
+			header1.appendChild(arrow);
+			
+			textNode = document.createTextNode(pageview);
+			
+			header1.appendChild(textNode);
+			container.appendChild(header1);			
+			body.appendChild(container);
+					 
+			var svg = d3.select('body')
+					.append('svg')
+					.attr('width', wdimensions[tab][pageview])
+					.attr('height', hdimensions[tab][pageview]);
+							
+			var g = svg.append('g');
+
+			//var scale = d3.scaleLinear().domain([max*0.1, max*0.5, max]).range(["white", "blue", "purple"]);			  
+			var scale = d3.scaleLinear().domain([max*0.05, max*0.1, max*0.2, max*0.55, max]).range(["white", "lightgreen", "green", "yellow", "red"]);
+		
+			var rects = g.selectAll('rect')
+						 .data(points[tab][pageview])
+						 .enter()
+						 .append('rect')
+						 .attr('x', function(d){
+							return d.x;
+						 })
+						 .attr('y', function(d){
+							return d.y;
+						 })
+						 .attr('width', square )
+						 .attr('height', square )
+						 .attr('fill', function(d){
+							return scale(d.fixation);
+						 })
+						 .style('opacity', function(d){
+							if( d.fixation > 0.05 ){ return 1.0 ; }
+								return 0 ;
+						 });
+						 
+			svg.attr('class', 'heatmap-d3');
+			svg.style('display', 'none');
+				
 				   
+		}
+		
+		
+
+	}
+	
+	$(".container-pageview").click(function(e) {
+			$(this).next(".heatmap-d3").stop().toggle();
+			$(this).find(".arrow-up, .arrow-down").toggle();
+			console.log('aa');
+			console.log(e);
+			});
+	
 				   
 	/*
 	var heatmap = h337.create({
@@ -405,5 +582,15 @@ function heatMap(loggerPack){
 			heatmap.addData({ x: point.x, y: point.y, value: point.fixation });
 		
 	}*/
+	
+	$.ajax({ url: 'https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp', success: function(data) { 
+	console.log(data);
+		html2canvas(data).then(function(canvas) {
+			document.body.appendChild(canvas);
+		}); 
+	} });
+
 									
 }
+
+
