75cdd0b3c44ec616cb493a8875f316d8c573a453
Felipe-Ferreira1 <felipe@DESKTOP-774POF2.localdomain>
2019-02-07 19:37:25 +0000

    patterns and incidents: new tables (predecessor -1 and successor + 1)

--- report/incidents.js
+++ report/incidents.js
@@ -108,225 +108,322 @@ function sheetsIncident(nodes, cardinality){
 	for(let node of nodes){
 		
 		if(node.sam == "incident"){
+				
+			var tab = node.tabId;
 			
-		var tab = node.tabId;
-		
-		if(tab != lastTab){
-			rowTab = document.createElement("tr");
-			table.appendChild(rowTab);
-			tabH = document.createElement("th");
-			tabH.className = "tabHeader";
-			tabH.colSpan = 3;
-			tabH.id = 'tab' + tab;
+			if(tab != lastTab){
+				rowTab = document.createElement("tr");
+				table.appendChild(rowTab);
+				tabH = document.createElement("th");
+				tabH.className = "tabHeader";
+				tabH.colSpan = 5;
+				tabH.id = 'tab' + tab;
+				
+				tabD = document.createTextNode('Tab ' + tab);
+				tabH.appendChild(tabD);
+				rowTab.appendChild(tabH);
+				
+				lastTab = tab;
+			}	
 			
-			tabD = document.createTextNode('Tab ' + tab);
-			tabH.appendChild(tabD);
-			rowTab.appendChild(tabH);
+			var links = node.links;
 			
-			lastTab = tab;
-		}	
-		
-		var links = node.links;
-		
-		var sources = [], targets = [];
-		
-		for(let x of links){
-			//console.log(x);
-			if(x.source == node.id && x.target != node.id){
-				targets.push(x.targetReference);
+			var sources = [], targets = [];
+			
+			for(let x of links){
+				
+				if(x.source == node.id && x.target != node.id){
+					targets.push(x.targetReference);
+				}
+				else if(x.target == node.id && x.source != node.id){
+					sources.push(x.sourceReference);
+				}
+				else{
+					/////////THINK
+					targets.push(x.sourceReference);
+				}
+				
 			}
-			else if(x.target == node.id && x.source != node.id){
-				sources.push(x.sourceReference);
+			
+			//getting the source and target nodes (from each source/target node of the event of interest)
+			var sources2 = [], targets2 = [];
+			var links2 = undefined;
+		
+			for(let y of sources){
+				links2 = y.links;
+				y.sources = [];
+			
+				for(let x of links2){
+					
+					if(x.target == y.id){
+						y.sources.push(x.sourceReference);
+					}
+				
+				}
+			
+			}
+		
+			for(let y of targets){
+				links2 = y.links;
+				y.targets = [];
+			
+				for(let x of links2){
+				
+					if(x.source == y.id){
+						y.targets.push(x.targetReference);
+					}
+				
+				}
+			
+			}
+			
+			var min, max;
+			var sumSource = 0, sumTarget = 0;
+			
+			if(sources.length < targets.length){
+				min = [sources.length, "sources"];
+				max = [targets.length, "targets"];
 			}
 			else{
-				/////////THINK
-				targets.push(x.sourceReference);
+				max = [sources.length, "sources"];
+				min = [targets.length, "targets"];
 			}
 			
-		}
-		
-		var min, max;
-		if(sources.length < targets.length){
-			min = [sources.length, "sources"];
-			max = [targets.length, "targets"];
-		}
-		else{
-			max = [sources.length, "sources"];
-			min = [targets.length, "targets"];
-		}
-		
-		var diff = max[0] - min[0];
-		
-		var tooltip, tooltiptxt, where;
-		var description, linebreak;
-		var boxSource, box, boxTarget;
-		var ts, tb, tt;
-		var sheetEoi, sheetSource, sheetTarget;
-		var row, rowSource, rowTarget;
-		var size = 50*max[0];
-		
-		
-		row = document.createElement("tr");
-		table.appendChild(row);
-		
-		//boxSource
-		boxSource = document.createElement("td");
-		boxSource.className = "boxSource";
-		boxSource.height = size;
-		row.appendChild(boxSource);
-		
-		ts = document.createElement("table");
-		ts.className = "innerTable";
-		ts.style.height = size + 'px';
-		boxSource.appendChild(ts);
-		
-		//box
-		box = document.createElement("td");
-		box.className = "box";
-		box.height = size;
-		row.appendChild(box);
-		
-		
-		tb = document.createElement("table");
-		tb.className = "innerTable";
-		tb.style.height = size + 'px';
-		box.appendChild(tb);
-		
-		//boxTarget
-		boxTarget = document.createElement("td");
-		boxTarget.className = "boxTarget";
-		boxTarget.height = size;
-		row.appendChild(boxTarget);
-		
-		tt = document.createElement("table");
-		tt.className = "innerTable";
-		tt.style.height = size + 'px';
-		boxTarget.appendChild(tt);
-		
+			for(let x of sources){
+				sumSource += x.sources.length;			
+			}
+			
+			for(let x of targets){
+				sumTarget += x.targets.length;
+			}		
+			
+			if(sumSource < sumTarget){
+				min = [sumSource, "sources"];
+				max = [sumTarget, "targets"];
+			}
+			else{
+				max = [sumSource, "sources"];
+				min = [sumTarget, "targets"];
+			}
+			
+			var diff = max[0] - min[0];
+			
+			var tooltip, tooltiptxt, where;
+			var description, linebreak;
+			var boxSource, box, boxTarget, boxSource1, boxTarget1;
+			var ts, tb, tt, ts1, tt1;
+			var sheetEoi, sheetSource, sheetTarget, sheetSource1, sheetTarget1;
+			var row, rowSource, rowTarget, rowSource1, rowTarget1;
+			var mHeight = 50;
+			var size = mHeight*max[0];
+			var padding = 24;	
+						
+			row = document.createElement("tr");
+			table.appendChild(row);
+			
+			//boxSource -1
+			boxSource1 = document.createElement("td");
+			boxSource1.className = "boxSource1";
+			boxSource1.height = size;
+			row.appendChild(boxSource1);
+			
+			ts1 = document.createElement("table");
+			ts1.className = "innerTable";
+			ts1.style.height = size + 'px';
+			boxSource1.appendChild(ts1);
+			
+			//boxSource
+			boxSource = document.createElement("td");
+			boxSource.className = "boxSource";
+			boxSource.height = size;
+			row.appendChild(boxSource);
+			
+			ts = document.createElement("table");
+			ts.className = "innerTable";
+			ts.style.height = size + 'px';
+			boxSource.appendChild(ts);
+			
+			//box
+			box = document.createElement("td");
+			box.className = "box";
+			box.height = size;
+			row.appendChild(box);
+			
+			
+			tb = document.createElement("table");
+			tb.className = "innerTable";
+			tb.style.height = size + 'px';
+			box.appendChild(tb);
+			
+			//boxTarget
+			boxTarget = document.createElement("td");
+			boxTarget.className = "boxTarget";
+			boxTarget.height = size;
+			row.appendChild(boxTarget);
+			
+			tt = document.createElement("table");
+			tt.className = "innerTable";
+			tt.style.height = size + 'px';
+			boxTarget.appendChild(tt);
+			
+			//boxTarget + 1
+			boxTarget1 = document.createElement("td");
+			boxTarget1.className = "boxTarget1";
+			boxTarget1.height = size;
+			row.appendChild(boxTarget1);
+			
+			tt1 = document.createElement("table");
+			tt1.className = "innerTable";
+			tt1.style.height = size + 'px';
+			boxTarget1.appendChild(tt1);
+			
 
-		//Node in box
-		row = document.createElement("tr");
-		row.className = "innerRow";
-		tb.appendChild(row);
-		
-		sheetEoi = document.createElement("td");
-		sheetEoi.className = "eoi";
-		sheetEoi.title = "Event: " + node.eventName + 
-							"\nOccurrences: " + node.occurrences +
-							"\nTab: " + node.tabId + 
-							"\nPath: " + node.path + 
-							"\nElement Id: " + node.elementId + 
-							"\nMean Distance: " + node.meanDistance.toFixed(2) + 
-							"\nMean Time: " + getTimestamp(node.meanTimestamp) +
-							"\nSam: " + node.sam;
-		sheetEoi.headers = 'header2Incident tab' + tab;
-		row.appendChild(sheetEoi);
-		
-		where = node.elementId;
-		if(where == '-') where = node.path;
-		description = document.createTextNode(node.eventName + '@' + where);
-		sheetEoi.appendChild(description);
-		
-		var j = 0, nodeSource, nodeTarget;
-		while(j < min[0]){
-			
-			nodeSource = sources[j];
-			nodeTarget = targets[j];
-			
-			rowSource = document.createElement("tr");
-			rowSource.className = "innerRow";
-			ts.appendChild(rowSource);
-
-			sheetSource = document.createElement("td");
-			sheetSource.className = "source";
-			sheetSource.title = "Event: " + nodeSource.eventName + 
-								"\nOccurrences: " + nodeSource.occurrences +
-								"\nTab: " + nodeSource.tabId + 
-								"\nPath: " + nodeSource.path + 
-								"\nElement Id: " +  nodeSource.elementId +
-								"\nMean Distance: " + nodeSource.meanDistance.toFixed(2) + 
-								"\nMean Time: " + getTimestamp(nodeSource.meanTimestamp) +
-								"\nSam: " + nodeSource.sam;
-			sheetSource.headers = 'header1Incident tab' + tab;
-			rowSource.appendChild(sheetSource);
-			
-			where = nodeSource.elementId;
-			if(where == '-') where = nodeSource.path;
-			description = document.createTextNode(nodeSource.eventName + '@' + where);
-			sheetSource.appendChild(description);
-			
-			rowTarget = document.createElement("tr");
-			rowTarget.className = "innerRow";
-			tt.appendChild(rowTarget);
-			
-			sheetTarget = document.createElement("td");
-			sheetTarget.className = "target";
-			sheetTarget.title = "Event: " + nodeTarget.eventName + 
-								"\nOccurrences: " + nodeTarget.occurrences +
-								"\nTab: " + nodeTarget.tabId + 
-								"\nPath: " + nodeTarget.path + 
-								"\nElement Id: " + nodeTarget.elementId +
-								"\nMean Distance: " + nodeTarget.meanDistance.toFixed(2) + 
-								"\nMean Time: " + getTimestamp(nodeTarget.meanTimestamp) +
-								"\nSam: " + nodeTarget.sam;
-			sheetTarget.headers = 'header3Incident tab' + tab;
-			rowTarget.appendChild(sheetTarget);
-			
-			where = nodeTarget.elementId;
-			if(where == '-') where = nodeTarget.path;
-			description = document.createTextNode(nodeTarget.eventName + '@' + where);
-			sheetTarget.appendChild(description);
-			
-		
-			j++;
-		}
-			
-		var array, className, t, sheet, header;
-		if(max[1] == "sources"){
-			array = sources;
-			className = "source";
-			t = ts;
-			header = 'header1Incident';
-		}
-		else{
-			array = targets;
-			className = "target";
-			t = tt;
-			header = 'header2Incident';
-		}
-		
-		var nodex;
-		while(j < max[0]){
-			
-			nodex = array[j];
-			
+			//Node in box
 			row = document.createElement("tr");
 			row.className = "innerRow";
-			t.appendChild(row);
+			tb.appendChild(row);
+			
+			sheetEoi = document.createElement("td");
+			sheetEoi.className = "eoi";
+			sheetEoi.title = "Event: " + node.eventName + 
+								"\nOccurrences: " + node.occurrences +
+								"\nTab: " + node.tabId + 
+								"\nPath: " + node.path + 
+								"\nElement Id: " + node.elementId + 
+								"\nMean Distance: " + node.meanDistance.toFixed(2) + 
+								"\nMean Time: " + getTimestamp(node.meanTimestamp) +
+								"\nSam: " + node.sam;
+			sheetEoi.headers = 'header2Incident tab' + tab;
+			row.appendChild(sheetEoi);
+			
+			where = node.elementId;
+			if(where == '-') where = node.path;
+			description = document.createTextNode(node.eventName + '@' + where);
+			sheetEoi.appendChild(description);
+			
+			var sumMin = 0;
+			var heightT, heightS;
+			
+			if(max[1] == 'targets'){
+				heightT = mHeight;
+				
+				for(let x of sources){
+					sumMin += x.sources.length;
+				}
+				heightS = size/sumMin;
+			}
+			else{
+				heightS = mHeight;
+				
+				for(let x of targets){
+					sumMin += x.targets.length;
+				}
+				heightT = size/sumMin;
+			}
+			
+			for(let x of sources){
+				links2 = x.sources;
+				
+				rowSource = document.createElement("tr");
+				rowSource.className = "innerRow";
+				ts.appendChild(rowSource);
 
-			sheet = document.createElement("td");
-			sheet.className = className;
-			sheet.title = "Event: " + nodex.eventName + 
-							"\nOccurrences: " + nodex.occurrences +
-							"\nTab: " + nodex.tabId + 
-							"\nPath: " + nodex.path + 
-							"\nElement Id: " + nodex.elementId +
-							"\nMean Distance: " + nodex.meanDistance.toFixed(2) + 
-							"\nMean Time: " + getTimestamp(nodex.meanTimestamp) +
-							"\nSam: " + nodex.sam;
-			sheet.width = "100%";
-			sheet.headers = header +  ' tab' + tab;
-			row.appendChild(sheet);
+				sheetSource = document.createElement("td");
+				sheetSource.style.height = (heightS*links2.length - padding) + 'px';
+				sheetSource.className = "source";
+				sheetSource.title = "Event: " + x.eventName + 
+									"\nOccurrences: " + x.occurrences +
+									"\nTab: " + x.tabId + 
+									"\nPath: " + x.path + 
+									"\nElement Id: " +  x.elementId +
+									"\nMean Distance: " + x.meanDistance.toFixed(2) + 
+									"\nMean Time: " + getTimestamp(x.meanTimestamp) +
+									"\nSam: " + x.sam;
+				sheetSource.headers = 'header1Incident tab' + tab;
+				rowSource.appendChild(sheetSource);
+				
+				where = x.elementId;
+				if(where == '-') where = x.path;
+				description = document.createTextNode(x.eventName + '@' + where);
+				sheetSource.appendChild(description);
+				
+				for(let y of links2){
+					
+					rowSource1 = document.createElement("tr");
+					rowSource1.className = "innerRow";
+					ts1.appendChild(rowSource1);
+					
+					sheetSource1 = document.createElement("td");
+					sheetSource1.className = "source1";
+					sheetSource1.title = "Event: " + y.eventName + 
+										"\nOccurrences: " + y.occurrences +
+										"\nTab: " + y.tabId + 
+										"\nPath: " + y.path + 
+										"\nElement Id: " +  y.elementId +
+										"\nMean Distance: " + y.meanDistance.toFixed(2) + 
+										"\nMean Time: " + getTimestamp(y.meanTimestamp) +
+										"\nSam: " + y.sam;
+					sheetSource1.headers = 'header0Incident tab' + tab;
+					rowSource1.appendChild(sheetSource1);
+					
+					where = y.elementId;
+					if(where == '-') where = y.path;
+					description = document.createTextNode(y.eventName + '@' + where);
+					sheetSource1.appendChild(description);
+				}
+			}
 			
-			where = nodex.elementId;
-			if(where == '-') where = nodex.path;
-			description = document.createTextNode(nodex.eventName + '@' + where);
-			sheet.appendChild(description);
-			
-			
-		
-			j++;
-		}
+			//target nodes
+			for(let x of targets){
+				links2 = x.targets;
+				
+				rowTarget = document.createElement("tr");
+				rowTarget.className = "innerRow";
+				tt.appendChild(rowTarget);
+				
+				sheetTarget = document.createElement("td");
+				sheetTarget.style.height = (heightT*links2.length - padding) + 'px';
+				sheetTarget.className = "target";
+				sheetTarget.title = "Event: " + x.eventName + 
+									"\nOccurrences: " + x.occurrences +
+									"\nTab: " + x.tabId + 
+									"\nPath: " + x.path + 
+									"\nElement Id: " + x.elementId +
+									"\nMean Distance: " + x.meanDistance.toFixed(2) + 
+									"\nMean Time: " + getTimestamp(x.meanTimestamp) +
+									"\nSam: " + x.sam;
+				sheetTarget.headers = 'header3Incident tab' + tab;
+				rowTarget.appendChild(sheetTarget);
+				
+				where = x.elementId;
+				if(where == '-') where = x.path;
+				description = document.createTextNode(x.eventName + '@' + where);
+				sheetTarget.appendChild(description);
+				
+				for(let y of links2){
+					rowTarget1 = document.createElement("tr");
+					rowTarget1.className = "innerRow";
+					tt1.appendChild(rowTarget1);
+					
+					sheetTarget1 = document.createElement("td");
+					sheetTarget1.className = "target1";
+					sheetTarget1.title = "Event: " + y.eventName + 
+										"\nOccurrences: " + y.occurrences +
+										"\nTab: " + y.tabId + 
+										"\nPath: " + y.path + 
+										"\nElement Id: " +  y.elementId +
+										"\nMean Distance: " + y.meanDistance.toFixed(2) + 
+										"\nMean Time: " + getTimestamp(y.meanTimestamp) +
+										"\nSam: " + y.sam;
+					sheetTarget1.headers = 'header4Incident tab' + tab;
+					rowTarget1.appendChild(sheetTarget1);
+					
+					where = y.elementId;
+					if(where == '-') where = y.path;
+					description = document.createTextNode(y.eventName + '@' + where);
+					sheetTarget1.appendChild(description);
+				}
+			}
 		}
 	}
 }
--- report/patterns.js
+++ report/patterns.js
@@ -111,7 +111,7 @@ function sheets(nodes, i, cardinality){
 	table.appendChild(rowTab);
 	var	tabH = document.createElement("th");
 	tabH.className = "tabHeader";
-	tabH.colSpan = 3;
+	tabH.colSpan = 5;
 	tabH.id = 'tab' + lastTab;
 	
 	var tabD = document.createTextNode('Tab ' + lastTab);
@@ -128,7 +128,7 @@ function sheets(nodes, i, cardinality){
 			table.appendChild(rowTab);
 			tabH = document.createElement("th");
 			tabH.className = "tabHeader";
-			tabH.colSpan = 3;
+			tabH.colSpan = 5;
 			tabH.id = 'tab' + tab;
 			
 			tabD = document.createTextNode('Tab ' + tab);
@@ -138,12 +138,14 @@ function sheets(nodes, i, cardinality){
 			lastTab = tab;
 		}		
 		
+		
+		//getting the source and target nodes
 		var links = node.links;
 		
 		var sources = [], targets = [];
 		
 		for(let x of links){
-			//console.log(x);
+			
 			if(x.source == node.id && x.target != node.id){
 				targets.push(x.targetReference);
 			}
@@ -157,29 +159,84 @@ function sheets(nodes, i, cardinality){
 			
 		}
 		
+		//getting the source and target nodes (from each source/target node of the event of interest)
+		var sources2 = [], targets2 = [];
+		var links2 = undefined;
+		
+		for(let y of sources){
+			links2 = y.links;
+			y.sources = [];
+			
+			for(let x of links2){
+				
+				if(x.target == y.id){
+					y.sources.push(x.sourceReference);
+				}
+			
+			}
+			
+		}
+		
+		for(let y of targets){
+			links2 = y.links;
+			y.targets = [];
+			
+			for(let x of links2){
+				
+				if(x.source == y.id){
+					y.targets.push(x.targetReference);
+				}
+				
+			}
+			
+		}
+		
 		var min, max;
-		if(sources.length < targets.length){
-			min = [sources.length, "sources"];
-			max = [targets.length, "targets"];
+		var sumSource = 0, sumTarget = 0;
+		
+		for(let x of sources){
+			sumSource += x.sources.length;			
+		}
+		
+		for(let x of targets){
+			sumTarget += x.targets.length;
+		}		
+		
+		if(sumSource < sumTarget){
+			min = [sumSource, "sources"];
+			max = [sumTarget, "targets"];
 		}
 		else{
-			max = [sources.length, "sources"];
-			min = [targets.length, "targets"];
+			max = [sumSource, "sources"];
+			min = [sumTarget, "targets"];
 		}
 		
 		var diff = max[0] - min[0];
 		
 		var tooltip, tooltiptxt, where;
 		var description, linebreak;
-		var boxSource, box, boxTarget;
-		var ts, tb, tt;
-		var sheetEoi, sheetSource, sheetTarget;
-		var row, rowSource, rowTarget;
-		var size = 50*max[0];
-		
-		
+		var boxSource, box, boxTarget, boxSource1, boxTarget1;
+		var ts, tb, tt, ts1, tt1;
+		var sheetEoi, sheetSource, sheetTarget, sheetSource1, sheetTarget1;
+		var row, rowSource, rowTarget, rowSource1, rowTarget1;
+		var mHeight = 50;
+		var size = mHeight*max[0];
+		var padding = 24;		
+				
 		row = document.createElement("tr");
 		table.appendChild(row);
+		
+		//boxSource -1
+		boxSource1 = document.createElement("td");
+		boxSource1.className = "boxSource1";
+		boxSource1.height = size;
+		row.appendChild(boxSource1);
+		
+		ts1 = document.createElement("table");
+		ts1.className = "innerTable";
+		ts1.style.height = size + 'px';
+		boxSource1.appendChild(ts1);
+		
 		
 		//boxSource
 		boxSource = document.createElement("td");
@@ -215,6 +272,16 @@ function sheets(nodes, i, cardinality){
 		tt.style.height = size + 'px';
 		boxTarget.appendChild(tt);
 		
+		//boxTarget + 1
+		boxTarget1 = document.createElement("td");
+		boxTarget1.className = "boxTarget1";
+		boxTarget1.height = size;
+		row.appendChild(boxTarget1);
+		
+		tt1 = document.createElement("table");
+		tt1.className = "innerTable";
+		tt1.style.height = size + 'px';
+		boxTarget1.appendChild(tt1);
 
 		//Node in box
 		row = document.createElement("tr");
@@ -239,105 +306,129 @@ function sheets(nodes, i, cardinality){
 		description = document.createTextNode(node.eventName + '@' + where);
 		sheetEoi.appendChild(description);
 		
-		var j = 0, nodeSource, nodeTarget;
-		while(j < min[0]){
+		var sumMin = 0;
+		var heightT, heightS;
+		
+		if(max[1] == 'targets'){
+			heightT = mHeight;
 			
-			nodeSource = sources[j];
-			nodeTarget = targets[j];
+			for(let x of sources){
+				sumMin += x.sources.length;
+			}
+			heightS = size/sumMin;
+		}
+		else{
+			heightS = mHeight;
+			
+			for(let x of targets){
+				sumMin += x.targets.length;
+			}
+			heightT = size/sumMin;
+		}
+				
+		//source -1 nodes
+		for(let x of sources){
+			links2 = x.sources;
 			
 			rowSource = document.createElement("tr");
 			rowSource.className = "innerRow";
 			ts.appendChild(rowSource);
 
 			sheetSource = document.createElement("td");
+			sheetSource.style.height = (heightS*links2.length - padding) + 'px';
 			sheetSource.className = "source";
-			sheetSource.title = "Event: " + nodeSource.eventName + 
-								"\nOccurrences: " + nodeSource.occurrences +
-								"\nTab: " + nodeSource.tabId + 
-								"\nPath: " + nodeSource.path + 
-								"\nElement Id: " +  nodeSource.elementId +
-								"\nMean Distance: " + nodeSource.meanDistance.toFixed(2) + 
-								"\nMean Time: " + getTimestamp(nodeSource.meanTimestamp) +
-								"\nSam: " + nodeSource.sam;
+			sheetSource.title = "Event: " + x.eventName + 
+								"\nOccurrences: " + x.occurrences +
+								"\nTab: " + x.tabId + 
+								"\nPath: " + x.path + 
+								"\nElement Id: " +  x.elementId +
+								"\nMean Distance: " + x.meanDistance.toFixed(2) + 
+								"\nMean Time: " + getTimestamp(x.meanTimestamp) +
+								"\nSam: " + x.sam;
 			sheetSource.headers = 'header1 tab' + tab;
 			rowSource.appendChild(sheetSource);
 			
-			where = nodeSource.elementId;
-			if(where == '-') where = nodeSource.path;
-			description = document.createTextNode(nodeSource.eventName + '@' + where);
+			where = x.elementId;
+			if(where == '-') where = x.path;
+			description = document.createTextNode(x.eventName + '@' + where);
 			sheetSource.appendChild(description);
+			
+			for(let y of links2){
+				
+				rowSource1 = document.createElement("tr");
+				rowSource1.className = "innerRow";
+				ts1.appendChild(rowSource1);
+				
+				sheetSource1 = document.createElement("td");
+				sheetSource1.className = "source1";
+				sheetSource1.title = "Event: " + y.eventName + 
+									"\nOccurrences: " + y.occurrences +
+									"\nTab: " + y.tabId + 
+									"\nPath: " + y.path + 
+									"\nElement Id: " +  y.elementId +
+									"\nMean Distance: " + y.meanDistance.toFixed(2) + 
+									"\nMean Time: " + getTimestamp(y.meanTimestamp) +
+									"\nSam: " + y.sam;
+				sheetSource1.headers = 'header0 tab' + tab;
+				rowSource1.appendChild(sheetSource1);
+				
+				where = y.elementId;
+				if(where == '-') where = y.path;
+				description = document.createTextNode(y.eventName + '@' + where);
+				sheetSource1.appendChild(description);
+			}
+		}
+		
+		//target nodes
+		for(let x of targets){
+			links2 = x.targets;
 			
 			rowTarget = document.createElement("tr");
 			rowTarget.className = "innerRow";
 			tt.appendChild(rowTarget);
 			
 			sheetTarget = document.createElement("td");
+			sheetTarget.style.height = (heightT*links2.length - padding) + 'px';
 			sheetTarget.className = "target";
-			sheetTarget.title = "Event: " + nodeTarget.eventName + 
-								"\nOccurrences: " + nodeTarget.occurrences +
-								"\nTab: " + nodeTarget.tabId + 
-								"\nPath: " + nodeTarget.path + 
-								"\nElement Id: " + nodeTarget.elementId +
-								"\nMean Distance: " + nodeTarget.meanDistance.toFixed(2) + 
-								"\nMean Time: " + getTimestamp(nodeTarget.meanTimestamp) +
-								"\nSam: " + nodeTarget.sam;
+			sheetTarget.title = "Event: " + x.eventName + 
+								"\nOccurrences: " + x.occurrences +
+								"\nTab: " + x.tabId + 
+								"\nPath: " + x.path + 
+								"\nElement Id: " + x.elementId +
+								"\nMean Distance: " + x.meanDistance.toFixed(2) + 
+								"\nMean Time: " + getTimestamp(x.meanTimestamp) +
+								"\nSam: " + x.sam;
 			sheetTarget.headers = 'header3 tab' + tab;
 			rowTarget.appendChild(sheetTarget);
 			
-			where = nodeTarget.elementId;
-			if(where == '-') where = nodeTarget.path;
-			description = document.createTextNode(nodeTarget.eventName + '@' + where);
+			where = x.elementId;
+			if(where == '-') where = x.path;
+			description = document.createTextNode(x.eventName + '@' + where);
 			sheetTarget.appendChild(description);
 			
-		
-			j++;
-		}
-			
-		var array, className, t, sheet, header;
-		if(max[1] == "sources"){
-			array = sources;
-			className = "source";
-			t = ts;
-			header = 'header1';
-		}
-		else{
-			array = targets;
-			className = "target";
-			t = tt;
-			header = 'header3';
-		}
-		
-		var nodex;
-		while(j < max[0]){
-			
-			nodex = array[j];
-			
-			row = document.createElement("tr");
-			row.className = "innerRow";
-			t.appendChild(row);
-
-			sheet = document.createElement("td");
-			sheet.className = className;
-			sheet.title = "Event: " + nodex.eventName + 
-							"\nOccurrences: " + nodex.occurrences +
-							"\nTab: " + nodex.tabId + 
-							"\nPath: " + nodex.path + 
-							"\nElement Id: " + nodex.elementId +
-							"\nMean Distance: " + nodex.meanDistance.toFixed(2) + 
-							"\nMean Time: " + getTimestamp(nodex.meanTimestamp) +
-							"\nSam: " + nodex.sam;
-			sheet.width = "100%";
-			sheet.headers = header +  ' tab' + tab;
-			row.appendChild(sheet);
-			
-			where = nodex.elementId;
-			if(where == '-') where = nodex.path;
-			description = document.createTextNode(nodex.eventName + '@' + where);
-			sheet.appendChild(description);
-			
-			
-		
-			j++;
+			for(let y of links2){
+				rowTarget1 = document.createElement("tr");
+				rowTarget1.className = "innerRow";
+				tt1.appendChild(rowTarget1);
+				
+				sheetTarget1 = document.createElement("td");
+				sheetTarget1.className = "target1";
+				sheetTarget1.title = "Event: " + y.eventName + 
+									"\nOccurrences: " + y.occurrences +
+									"\nTab: " + y.tabId + 
+									"\nPath: " + y.path + 
+									"\nElement Id: " +  y.elementId +
+									"\nMean Distance: " + y.meanDistance.toFixed(2) + 
+									"\nMean Time: " + getTimestamp(y.meanTimestamp) +
+									"\nSam: " + y.sam;
+				sheetTarget1.headers = 'header4 tab' + tab;
+				rowTarget1.appendChild(sheetTarget1);
+				
+				where = y.elementId;
+				if(where == '-') where = y.path;
+				description = document.createTextNode(y.eventName + '@' + where);
+				sheetTarget1.appendChild(description);
+			}
 		}
 		
 	}
